e92404dc44e58accb19481f3a13ad84ff93ffa20
BoyBoyDaBig <75626566+YannickSz@users.noreply.github.com>
2022-07-13 17:52:00 +0200

    updated Lager and Automat controllers

--- app/src/main/java/com/example/mysnackautomatapp/dbController/DBControllerAutomat.java
+++ app/src/main/java/com/example/mysnackautomatapp/dbController/DBControllerAutomat.java
@@ -1,6 +1,8 @@
 package com.example.mysnackautomatapp.dbController;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -9,8 +11,18 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 import java.sql.SQLClientInfoException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class DBControllerAutomat extends SQLiteOpenHelper {
+    private static final String tablename = "tblAutomatProducts"; // tablename
+    private static final String productID = "productID"; // auto generated ID column
+    private static final String amount = "amount"; // column name
+    private static final String sellPrice = "sellPrice"; // column name
+    private static final String closestMHD = "closestMHD"; // column name
+    private static final String databasename = "dbProducts"; // Dtabasename
+    private static final int versioncode = 1; //versioncode of the database
+
     public DBControllerAutomat(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
@@ -24,12 +36,59 @@ public class DBControllerAutomat extends SQLiteOpenHelper {
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+    public void onCreate(SQLiteDatabase database) {
+        String query;
+        query = "CREATE TABLE IF NOT EXISTS " + tablename + "(" + productID + " integer primary key, "
+                + amount + " text, " + sellPrice + " text, " + closestMHD + "  text )";
+        database.execSQL(query);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+    public void onUpgrade(SQLiteDatabase database, int version_old, int current_version) {
+        String query;
+        query = "DROP TABLE IF EXISTS " + tablename;
+        database.execSQL(query);
+        onCreate(database);
     }
+
+    public ArrayList<HashMap<String, String>> getAutomatProducts() {
+
+        ArrayList<HashMap<String, String>> productAutomatList = new ArrayList<HashMap<String, String>>();
+        SQLiteDatabase database = this.getWritableDatabase();
+        Cursor cursor = database.rawQuery("SELECT * FROM " + tablename ,null);
+        if (cursor.moveToFirst()) {
+            do {
+
+                HashMap<String, String> map = new HashMap<String, String>();
+                map.put("productID", cursor.getString(0));
+                map.put("amount", cursor.getString(1));
+                map.put("sellPrice", cursor.getString(2));
+                map.put("closestMHD", cursor.getString(3));
+                productAutomatList.add(map);
+            } while (cursor.moveToNext());
+        }
+
+        cursor.close();
+        database.close();
+
+// return contact list
+        return productAutomatList;
+    }
+
+    public boolean addAutomatProduct(String productAmount, String productPrice, String productMHD) {
+        try {
+            SQLiteDatabase db = this.getWritableDatabase();
+            ContentValues cv = new ContentValues();
+            cv.put(amount, productAmount);
+            cv.put(sellPrice, productPrice);
+            cv.put(closestMHD, productMHD);
+            db.insert(tablename, null, cv);
+            db.close();
+
+            return true;
+        } catch (Exception ex) {
+            return false;
+        }
+    }
+
 }
--- app/src/main/java/com/example/mysnackautomatapp/dbController/DBControllerLager.java
+++ app/src/main/java/com/example/mysnackautomatapp/dbController/DBControllerLager.java
@@ -1,6 +1,8 @@
 package com.example.mysnackautomatapp.dbController;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.DatabaseErrorHandler;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -8,7 +10,17 @@ import android.database.sqlite.SQLiteOpenHelper;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class DBControllerLager extends SQLiteOpenHelper {
+    private static final String tablename = "tblLagerProducts"; // tablename
+    private static final String productID = "productID"; // column name
+    private static final String amount = "amount"; // auto generated ID column
+    private static final String closestMHD = "closestMHD"; // column name
+    private static final String databasename = "dbProducts"; // Dtabasename
+    private static final int versioncode = 1; //versioncode of the database
+
     public DBControllerLager(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
         super(context, name, factory, version);
     }
@@ -22,12 +34,57 @@ public class DBControllerLager extends SQLiteOpenHelper {
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+    public void onCreate(SQLiteDatabase database) {
+        String query;
+        query = "CREATE TABLE IF NOT EXISTS " + tablename + "(" + productID + " integer primary key, "
+                + amount + " text, " + closestMHD + "  text )";
+        database.execSQL(query);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+    public void onUpgrade(SQLiteDatabase database, int i, int i1) {
+        String query;
+        query = "DROP TABLE IF EXISTS " + tablename;
+        database.execSQL(query);
+        onCreate(database);
     }
+
+    public ArrayList<HashMap<String, String>> getLagerProducts() {
+
+        ArrayList<HashMap<String, String>> productLagerList = new ArrayList<HashMap<String, String>>();
+        SQLiteDatabase database = this.getWritableDatabase();
+        Cursor cursor = database.rawQuery("SELECT * FROM " + tablename ,null);
+        if (cursor.moveToFirst()) {
+            do {
+
+                HashMap<String, String> map = new HashMap<String, String>();
+                map.put("productID", cursor.getString(0));
+                map.put("amount", cursor.getString(1));
+                map.put("closestMHD", cursor.getString(2));
+                productLagerList.add(map);
+            } while (cursor.moveToNext());
+        }
+
+        cursor.close();
+        database.close();
+
+// return contact list
+        return productLagerList;
+    }
+
+    public boolean addLagerProduct(String productAmount, String productMHD) {
+        try {
+            SQLiteDatabase db = this.getWritableDatabase();
+            ContentValues cv = new ContentValues();
+            cv.put(amount, productAmount);
+            cv.put(closestMHD, productMHD);
+            db.insert(tablename, null, cv);
+            db.close();
+
+            return true;
+        } catch (Exception ex) {
+            return false;
+        }
+    }
+
 }
